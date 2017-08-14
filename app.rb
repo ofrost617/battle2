1f68d72c6d136aeb73f1d0834f8b0c8b39f318aa
alessiobortone2 <alessiobortone2@gmail.com>
2017-08-14 15:47:55 +0100

    Sinatra views

--- app.rb
+++ app.rb
@@ -14,7 +14,6 @@ get '/secretpath' do
 end
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  @name = ["Alessio","Puyan"].sample
+  erb(:index)
 end
