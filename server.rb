5a766c155e78d8cc52af94b9e64bfc0e16db214c
alessiobortone2 <alessiobortone2@gmail.com>
2017-08-16 15:09:23 +0100

    Start of a lovely afternoon / Abi

--- server.rb
+++ server.rb
@@ -1,24 +1,24 @@
 require 'socket'
 
-server = TCPServer.new("192.168.48.126", 2345)
+server = TCPServer.new('localhost', 2345)
 
 socket = server.accept
 
 loop do
-socket.puts "What game would you like to play?"
-socket.puts "1. Chess"
-socket.puts "2. Tic Tac Toe"
-socket.puts "3. Global Thermonuclear War"
-socket.puts "Chose 1,2 or 3 (or quit)"
+  socket.puts "What game would you like to play?"
+  socket.puts "1. Chess"
+  socket.puts "2. Tic Tac Toe"
+  socket.puts "3. Global Thermonuclear War"
+  socket.puts "Chose 1,2 or 3 (or quit)"
 
-they_said = socket.gets.chomp
+  they_said = socket.gets.chomp
 
-socket.puts "Hi Donald!" if they_said == "3"
-socket.puts "why not 3?" if they_said == "2"
-socket.puts "why not 3?" if they_said == "1"
-socket.puts "You chose: #{they_said}!"
+  socket.puts "Hi Donald!" if they_said == "3"
+  socket.puts "why not 3?" if they_said == "2"
+  socket.puts "why not 3?" if they_said == "1"
+  socket.puts "You chose: #{they_said}!"
 
-exit if they_said == 'quit'
+  exit if they_said == 'quit'
 
 end
 socket.close
