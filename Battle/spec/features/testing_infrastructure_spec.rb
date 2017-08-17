a17cfbfdd799c7dc3d54fc5fe69f944f9dc1a8b6
Olivia Frost <olivia.frost617@googlemail.com>
2017-08-17 15:45:46 +0100

    Test helpers complete

--- Battle/spec/features/testing_infrastructure_spec.rb
+++ Battle/spec/features/testing_infrastructure_spec.rb
@@ -1,24 +1,20 @@
 require './app.rb'
+require_relative './web_helpers.rb'
 
 feature Battle do
+
   scenario 'it asks to enter players' do
     visit('/')
     expect(page).to have_content 'Enter players'
   end
 
   scenario 'fill and check page content' do
-    visit('/')
-    fill_in('player1', :with => 'Alessio')
-    fill_in('player2', :with => 'Olivia')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Alessio and Olivia are going to battle!'
   end
 
   scenario 'player 1 can see player 2 hit points' do
-    visit('/')
-    fill_in('player1', :with => 'Alessio')
-    fill_in('player2', :with => 'Olivia')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Olivia points: 20'
   end
 
